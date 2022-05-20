a7f8a15f9918cf80146036d72970f3d5bd413486
ViktoriiaTverezovska <viktoriia@smartersorting.com>
2022-05-20 15:52:55 -0600

    Test data for PH null validation
    Currently tests contain full response.
    Might need to find a way to verify ONLY PH validation part instead of the full response.

--- data-qa/delphi/test/genome-validation/PHnullValidation.spec.js
+++ data-qa/delphi/test/genome-validation/PHnullValidation.spec.js
@@ -3,22 +3,25 @@ let config = require('../../../support/setUp.js');
 let commons = require('../../../support/helpers.js');
 
 describe('Validate genome - PH null validation tests', function () {
-  context(
-    'Validate that sending request in correct format expected response will be received',
-    function () {
-      Object.entries(testData.variants).forEach(([variant, data]) => {
-        it(`Should get a proper response with ${variant}`, async () => {
-          const res = await commons.postRequest(
-            config.interpretersBaseUrl,
-            testData.uri,
-            data.request,
-            '',
-          );
-          expect(res.statusCode).to.be.equal(data.responseCode);
-          //expect(res.body).to.be.deep.equal(data.response); //we are not verifying the full response
-          expect(res.body).to.include(data.response);
-        });
-      });
-    },
-  );
+    context(
+        'Validate that sending request in correct format expected response will be received',
+        function () {
+            Object.entries(testData.variants).forEach(([variant, data]) => {
+                it(`Should get a proper response with ${variant}`, async () => {
+                    const res = await commons.postRequest(
+                        config.interpretersBaseUrl,
+                        testData.uri,
+                        data.request,
+                        '',
+                    );
+                    expect(res.statusCode).to.be.equal(data.responseCode);
+                    expect(res.body).to.be.deep.equal(data.response); //we are not verifying the full response
+                    //expect(res.body).to.deep.include(data.response);
+                    
+                    //console.log(res.body);
+                    //expect(res.body).to.have.deep.nested.property('observations.normalized_value.ph_details.validation_tests', data.response);
+                });
+            });
+        },
+    );
 });
