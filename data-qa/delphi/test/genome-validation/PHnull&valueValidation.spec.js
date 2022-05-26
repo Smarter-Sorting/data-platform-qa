ff76facf2eeae771505967d41d74d40405c7201f
ViktoriiaTverezovska <viktoriia@smartersorting.com>
2022-05-26 13:51:47 -0600

    added function

--- data-qa/delphi/test/genome-validation/PHnull&valueValidation.spec.js
+++ data-qa/delphi/test/genome-validation/PHnull&valueValidation.spec.js
@@ -3,41 +3,33 @@ let phValueValidationTestData = require('../../fixtures/genome-validation/PHvalu
 let config = require('../../../support/setUp.js');
 let commons = require('../../../support/helpers.js');
 
-describe('Validate genome - PH validation tests', function () {
+describe('Validate genome - PH validation tests', () => {
     context(
         'PH null validation: Validate that sending request in correct format expected response will be received',
-        function () {
-            Object.entries(phNullValidationTestData.variants).forEach(([variant, data]) => {
-                it(`Should get a proper response with ${variant}`, async () => {
-                    const res = await commons.postRequest(
-                        config.interpretersBaseUrl,
-                        phNullValidationTestData.uri,
-                        data.request,
-                        '',
-                    );
-                    expect(res.statusCode).to.be.equal(data.responseCode);
-                    expect(res.body.observations[0].normalized_value.ph).to.be.deep.equal(data.response_ph);
-                    expect(res.body.observations[0].normalized_value.ph_details).to.be.deep.equal(data.response_ph_details);
-                });
-            });
+        () => {
+            phValidation(phNullValidationTestData);
         },
     );
     context(
         'PH value validation: Validate that sending request in correct format expected response will be received',
-        function () {
-            Object.entries(phValueValidationTestData.variants).forEach(([variant, data]) => {
-                it(`Should get a proper response with ${variant}`, async () => {
-                    const res = await commons.postRequest(
-                        config.interpretersBaseUrl,
-                        phValueValidationTestData.uri,
-                        data.request,
-                        '',
-                    );
-                    expect(res.statusCode).to.be.equal(data.responseCode);
-                    expect(res.body.observations[0].normalized_value.ph).to.be.deep.equal(data.response_ph);
-                    expect(res.body.observations[0].normalized_value.ph_details).to.be.deep.equal(data.response_ph_details);
-                });
-            });
+        () => {
+            phValidation(phValueValidationTestData);
         },
     );
 });
+
+function phValidation(testData) {
+    Object.entries(testData.variants).forEach(([variant, data]) => {
+        it(`Should get a proper response with ${variant}`, async () => {
+            const res = await commons.postRequest(
+                config.interpretersBaseUrl,
+                testData.uri,
+                data.request,
+                '',
+            );
+            expect(res.statusCode).to.be.equal(data.responseCode);
+            expect(res.body.observations[0].normalized_value.ph).to.be.deep.equal(data.response_ph);
+            expect(res.body.observations[0].normalized_value.ph_details).to.be.deep.equal(data.response_ph_details);
+        });
+    });
+}
