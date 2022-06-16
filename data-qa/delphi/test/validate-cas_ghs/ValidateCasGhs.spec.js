463ffaa58f1565d32837dc69b0755fd8879f1e1b
ViktoriiaTverezovska <viktoriia@smartersorting.com>
2022-06-16 08:36:25 -0600

    Test files + first two tests

---
+++ data-qa/delphi/test/validate-cas_ghs/ValidateCasGhs.spec.js
@@ -0,0 +1,26 @@
+let testData = require('../../fixtures/validate-cas_ghs/ValidateCasGhsTestData.json');
+let config = require('../../../support/setUp.js');
+let commons = require('../../../support/helpers.js');
+
+describe('Validate CAS - GHS tests', function () {
+    context(
+        'Validate that sending request in correct format expected response will be received',
+        function () {
+            Object.entries(testData.variants).forEach(([variant, data]) => {
+                it(`Should get a proper response with ${variant}`, async () => {
+                    const res = await commons.postRequest(
+                        config.interpretersBaseUrl,
+                        testData.uri,
+                        data.request,
+                        '',
+                    );
+                    expect(res.statusCode).to.be.equal(data.responseCode);
+                    expect(res.body).to.be.deep.equal(data.response); //some responces have ingrdients listed in different order so deep.equal fails
+                    //expect(res.body).to.deep.equalInAnyOrder(data.response);
+                   
+                    
+                });
+            });
+        },
+    );
+});
