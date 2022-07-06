57bca6b1d4659574d939380044c1f860f19b4aa1
ViktoriiaTverezovska <viktoriia@smartersorting.com>
2022-07-06 13:54:18 -0500

    test Ph calculator

--- data-qa/support/setUp.js
+++ data-qa/support/setUp.js
@@ -4,10 +4,23 @@ const localhostFactory = process.env.LOCALHOST_FACTORY;
 const mlBaseUrl = process.env.ML_BASE_URL;
 const mlToken = process.env.ML_TOKEN;
 
+const testRailDomain = process.env.TESTRAIL_DOMAIN
+const testRailUsername = process.env.TESTRAIL_USERNAME
+const testRailPassword = process.env.TESTRAIL_PASSWORD
+const testRailProjectId = process.env.TESTRAIL_PROJECTID
+const testRailSuiteId = process.env.TESTRAIL_SUITEID
+const testRailAssignedToId = process.env.TESTRAIL_ASSIGNEDTOID
+
 module.exports = {
   interpretersBaseUrl,
   localhostBinning,
   localhostFactory,
   mlBaseUrl,
   mlToken,
+  testRailDomain,
+  testRailUsername,
+  testRailPassword,
+  testRailProjectId,
+  testRailSuiteId,
+  testRailAssignedToId
 };
